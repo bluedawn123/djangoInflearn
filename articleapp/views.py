e4ee62748bc88fc5649413e30f3c3b2dbb477d2a
haemilyjh <haemilyjh@naver.com>
2021-09-01 02:51:28 +0900

    chapter 35

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,50 @@
+
+
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView, UpdateView
+
+from articleapp.decorators import article_ownership_required
+from articleapp.forms import ArticleCreationForm
+from articleapp.models import Article
+
+
+
+#게시글을 작성 할때에는 로그인이 필요하기 때문
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    template_name = 'articleapp/create.html'
+
+    def form_valid(self, form):
+        temp_article = form.save(commit=False)
+        temp_article.writer = self.request.user
+        temp_article.save()
+        
+        return super().form_valid(form)    #위에것이 아니면 그대로
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
+
+class ArticleDetailView(DetailView):
+    model = Article
+    context_object_name = 'target_article'
+    template_name = 'articleapp/detail.html'
+
+# update에는 주인인지 아닌지가 필요해서 인증시스템이 필요 => decorator사용
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    template_name = 'articleapp/update.html'
+    context_object_name = 'target_article'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
